9d807e703131088ca1b9bf33bcba51ded67c2699
Frederick Morrison <frederick.morrison@outlook.com>
2023-12-13 14:10:18 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+#
+import sys
+import csv
+
+
+def get_python_version() -> str:
+    return f'{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}'
+
+
+def write_csv_file(filename: str):
+    with open(filename, 'w', newline='') as csvfile:
+        # prepare the header row
+        fieldnames = ['first_name', 'last_name']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+        # write a few data rows
+        writer.writerow({'first_name': 'Mark', 'last_name': 'Grohman'})
+        writer.writerow({'first_name': 'Frederick', 'last_name': 'Morrison'})
+        writer.writerow({'first_name': 'John', 'last_name': 'Smith'})
+
+
+def echo_csv_file(filename: str):
+    with open(filename) as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            print(f"{row['first_name']} {row['last_name'].strip()} ")
+
+
+if __name__ == '__main__':
+    print(f'Python version {get_python_version()}')
+    filename = 'names.csv'
+    write_csv_file(filename)
+    echo_csv_file(filename)
