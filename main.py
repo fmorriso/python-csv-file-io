61b2bc0c2dada5f3dd4b79d88d8463a3ff9aaf44
Fred Morrison <frederick.morrison@outlook.com>
2025-05-24 11:12:05 -0600

    reformat code

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-import sys
 import csv
+import sys
 
 
 def get_python_version() -> str:
     return f'{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}'
 
 
-def write_csv_file(file_name: str):
-    with open(file_name, 'w', newline= '') as csvfile:
+def write_csv_file(file_name: str) -> None:
+    with open(file_name, 'w', newline = '') as csvfile:
         # prepare the header row
         fieldnames = ['first_name', 'last_name']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
         writer.writeheader()
 
         # write a few data rows to the CSV file
@@ -19,7 +19,7 @@ def write_csv_file(file_name: str):
         writer.writerow({'first_name': 'John', 'last_name': 'Smith'})
 
 
-def echo_csv_file(file_name: str):
+def echo_csv_file(file_name: str) -> None:
     with open(file_name) as f:
         reader = csv.DictReader(f)
         for row in reader:
